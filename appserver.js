6d7ce69fcd0c717bf71cbad4c57460fcfa3492d5
shubham200906 <shubham200906@gmail.com>
2025-09-05 20:23:55 -0700

    Implemented Backend Programming and Transferred Core Logic to Backend

---
+++ appserver.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors({
+  origin : "http://localhost:5173"
+}));
+
+const info = {
+    username : "Shubham",
+    password : "hello"
+};
+
+app.get("/", (req, res) => {
+
+  if(req.query.username != info["username"] && req.query.password != info["password"]) {
+    res.send("Incorrect Username and Password");
+  } else if(req.query.username != info["username"]) {
+   res.send("Incorrect Username");
+  } else if(req.query.password != info["password"]) {
+    res.send("Incorrect Password");
+  } else {
+    res.send("Correct Username & Password");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,66 @@
-import { StrictMode, useState } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 function MyApp() {
 
-  const info = {
-    username : "Shubham",
-    password : "hello"
-  }
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [userTrue, setUserTrue] = useState(true);
   const [passTrue, setPassTrue] = useState(true);
 
-  function formSubmit(event) {
+  const [data, setData] = useState(true);
 
+  function formSubmit(event) {
     event.preventDefault();
 
-    if(username != info['username']) {
-      console.log("Incorrect Username");
-      setUserTrue(false);
-    } else {
-      console.log("Correct Username");
-      setUserTrue(true);
-    }
+    fetch(`http://localhost:3000/?username=${username}&password=${password}`, {
+      method: "GET",
+    })
+    .then(response => response.text())
+    .then(result => {
+      
+      setData(result);
+      console.log(result);
 
-    
-    if(password != info['password']) {
-      console.log("Incorrect Password");
-      setPassTrue(false);
-    } else {
-      console.log("Correct Password");
-      setPassTrue(true);
-    }
+      if(result.includes("Incorrect Username")) {
+        setUserTrue(false);
+      }
+
+      if(result.includes("Incorrect Password")) {
+        setPassTrue(false);
+      }
+
+      if(result.includes("Incorrect Username and Password")) {
+        setUserTrue(false);
+        setPassTrue(false);
+      }
+
+      if(result.includes("Correct Username and Password")) {
+        setUserTrue(true);
+        setPassTrue(true);
+      }
+    });
   }
 
   function handleUsername(event) {
     setUsername(event.target.value);
+    setUserTrue(true);
   }
 
   function handlePassword(event) {
     setPassword(event.target.value);
+    setPassTrue(true);
   }
 
+  // in formsubmit, bring it back to the code it was before (it should perform the code locally - you have info in react)
+  // send request to app.js (call localhost:3000), and in app.js create method called validateUser from the client and sends a response saying valid/invalid username/password
+
   return (
-    <form onSubmit={formSubmit}>
-      <label class="fs-4">Username: <input type='text' onChange={handleUsername} class="form-control size fs-4 rounded-top rounded-bottom" className={userTrue ? "bg-white" : "bg-danger"}/></label><br /><br />
-      <label class="fs-4">Password: <input type='password' onChange={handlePassword} class="form-control size fs-4 rounded-top rounded-bottom" className={passTrue ? "bg-white" : "bg-danger"}/></label><br /><br />
+    <form onSubmit={formSubmit} method="get">
+      <label class="fs-4">Username: <input type='text' onChange={handleUsername} className={`form-control size fs-4 rounded-top rounded-bottom ${userTrue ? "bg-white" : "bg-danger"}`} name="username"/></label><br /><br />
+      <label class="fs-4">Password: <input type='password' onChange={handlePassword} className={`form-control size fs-4 rounded-top rounded-bottom" ${passTrue ? "bg-white" : "bg-danger"}`} name="password"/></label><br /><br />
       <input type='submit' class="btn btn-primary btn-lg size fs-4 button-hover rounded-top rounded-bottom"/>
     </form>
   );
